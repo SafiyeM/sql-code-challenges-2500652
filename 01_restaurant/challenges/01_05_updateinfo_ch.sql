29c4d29533890a45f6bfde607412bc001fdf5f00
Safiye Mertcan <122940752+SafiyeM@users.noreply.github.com>
2023-12-24 12:55:58 +0000

    update

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,14 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+
+SELECT KundenID, Vorname, Nachname, Adresse
+from Kunden
+where Vorname = 'Taylor' and Nachname = 'Jenkins';
+
+UPDATE Kunden 
+set Adresse = '74 Pine St.', Stadt = 'New York', state = 'NY'
+where KundenID = 26;
+
+SELECT * from Kunden
+where KundenID = 26;
