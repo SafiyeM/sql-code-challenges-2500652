c70f39d4ec21c99b254383a812a9f7c0fbb6811c
Safiye Mertcan <122940752+SafiyeM@users.noreply.github.com>
2023-12-26 22:45:38 +0000

    restaurant challenge completed

--- 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
+++ 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
@@ -2,3 +2,17 @@
 
 -- Tragen Sie Cleo Goldwaters Lieblingsgericht 
 -- als "Quinoa Salmon Salad" ein.
+
+-- This query gives me 9 as SpeiseID of Quinoa Salmon Salad
+SELECT SpeiseID from Speise
+where Name = 'Quinoa Salmon Salad';
+
+SELECT * from Kunden
+where Vorname = 'Cleo' AND Nachname = 'Goldwater';
+
+UPDATE Kunden
+SET LieblingsGericht = 9
+WHERE KundenID = 42 ;
+
+
+
--- 01_restaurant/challenges/01_12_bestekunden_ch.sql
+++ 01_restaurant/challenges/01_12_bestekunden_ch.sql
@@ -4,3 +4,35 @@
 
 -- Geben Sie die Anzahl der Bestellungen, 
 -- den Vornamen, Nachnamen sowie die Email-Adresse an.
+
+
+SELECT COUNT(Bestellungen.BestellungsID) as AnzahlBestellungen, Kunden.Vorname, Kunden.Nachname, Kunden.Email
+FROM Bestellungen
+join Kunden on Kunden.KundenID = Bestellungen.KundenID
+GROUP By Bestellungen.KundenID
+ORDER BY AnzahlBestellungen desc;
+
+
+
+-- Ekstra Just for fun
+SELECT 
+       count(Bestellungen.BestellungsID) as AnzahlBestellungen,
+       Kunden.KundenID, 
+       Kunden.Vorname, 
+       Kunden.Nachname, 
+       Kunden.Email
+from 
+      Bestellungen
+JOIN 
+      Kunden on Kunden.KundenID = Bestellungen.KundenID
+WHERE 
+      Kunden.KundenID = 2;
+-- Output -  AnzahlBestellungen	 KundenID	   Vorname	 Nachname	   Email
+--                   14	             2	      Javier	  Dawks	   jdawks1@red30design.com
+
+
+
+-- Just for fun 
+SELECT count(Speise.Name) from Speise
+Join BestellungSpeise on BestellungSpeise.SpeiseID = Speise.SpeiseID
+where name = 'Handcrafted Pizza'; -- Handcrafted Pizza 193 times is ordered.
