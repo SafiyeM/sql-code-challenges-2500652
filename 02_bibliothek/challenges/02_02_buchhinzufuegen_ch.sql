b9079f393827d1b3bb939e5e61d7f5c6df89e5a0
Safiye Mertcan <122940752+SafiyeM@users.noreply.github.com>
2023-12-28 20:25:55 +0000

    new commits

--- 02_bibliothek/challenges/02_02_buchhinzufuegen_ch.sql
+++ 02_bibliothek/challenges/02_02_buchhinzufuegen_ch.sql
@@ -1,6 +1,9 @@
 -- Fügen Sie folgende Bücher in die Datenbank hinzu
 -- und stellen diese zum Ausleihen bereit.
 
+-- Add the following books to the database
+-- and make them available for borrowing.
+
 /*
 - Titel: Dracula  
 - Autor: Bram Stoker 
@@ -11,3 +14,13 @@
 - Autor: Jonathan Swift
 - Jahr: 1729
 - Neue ID: 4899254401 */
+
+
+Insert INTO Buecher(Titel, AutorIn, Erscheinungsdatum, Barcode)
+VALUES('Dracula', 'Bram Stoker', 1729, 4819277482), 
+      ('Gullivers Travels into Several Remote Nations of the World', 'Jonathan Swift', 1729, 4899254401 );
+
+SELECT * from Buecher
+order by BuchID desc;
+
+
